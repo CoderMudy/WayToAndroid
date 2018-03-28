fabf6b447c918c94eb988daad8970a9020556248
12东永 <2808050775@qq.com>
2018-03-28 15:06:37 +0800

    常用适配器BaseAdapter方法

--- Day05/02_BaseAdapter的应用/app/src/main/java/listdemo/com/mudy/a02_baseadapter/MyAdapter.java
+++ Day05/02_BaseAdapter的应用/app/src/main/java/listdemo/com/mudy/a02_baseadapter/MyAdapter.java
@@ -1,5 +1,6 @@
 package listdemo.com.mudy.a02_baseadapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,25 +35,43 @@ public class MyAdapter extends BaseAdapter{
         return 0;
     }
 
+    /**
+     *
+     * @param position 相当于当前的索引
+     * @param convertView 表示的就是缓存view
+     * @param parent 表示一个容器 每个itemview里边的容器 返回的View直接添加到容器上
+     * @return 就是每一个ItemView要显示的内容
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //TextView tv = new TextView(parent.getContext());
-        //tv.setLayoutParams(new AbsListView.LayoutParams(-1,-2));
-        //tv.setTextSize(15);
-        //tv.setText(mDatas.get(position));
+        Log.v("mudy","convertView=" + convertView + "  position" + position);
 
         //LayoutInflater 布局充气的一个类，就是可以将一个布局转换成一个view
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.lv_item_layout,null);
-        TextView tv = (TextView)view.findViewById(R.id.tv);
+        View view = layoutInflater.inflate(android.R.layout.simple_list_item_checked,null);//如果只有一个textview的话，可以使用安卓提供的这个控件
+        TextView tv = (TextView)view.findViewById(android.R.id.text1);
         tv.setText(mDatas.get(position));
-        return tv;
+
+        //TextView tv2 = (TextView)view.findViewById(android.R.id.text2);
+        //tv2.setText(mDatas.get(position));
+        return view;
     }
 }
 
 /**
  * 每一个view都可以获取context  v.getContext()
  *
- * 通过布局文件直接转换成view
+ * 通过布局文件直接转换成view  layoutInflater.inflate()
+ *
+ * 通过某个容器conteiner找到它的子控件: view.findViewById();
+ *
+ *
+ * * 安卓已经帮我们定义好了的一些子项目布局
+ * android.R.layout.simple_list_item_checked 有一个对勾的
+ *
+ * android.R.layout.simple_list_item_2 有子标题的
+ *
+ *
+ *
  *
  */
