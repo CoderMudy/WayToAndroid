90a971013789ea750bd1fb40493b33f645b8f2c0
12东永 <2808050775@qq.com>
2018-04-04 14:22:15 +0800

    fragment和activity生命周期，待详细补充

--- P1_Day01/fragment/app/src/main/java/fragment/com/mudy/fragment/FirstFragment.java
+++ P1_Day01/fragment/app/src/main/java/fragment/com/mudy/fragment/FirstFragment.java
@@ -1,7 +1,9 @@
 package fragment.com.mudy.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 public class FirstFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.v("mudy","Fragment-onCreateView");
         View view = inflater.inflate(R.layout.fragment_first,null);
         Bundle bundle = getArguments();
         String title = bundle.getString("title");
@@ -21,5 +24,36 @@ public class FirstFragment extends Fragment{
         textView.setText(title);
 
         return view;
+    }
+
+    //代表Fragment和一个Activity开始发生超友谊关系
+    @Override
+    public void onAttach(Context context) {
+        Log.v("mudy","Fragment-onAttach");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.v("mudy","Fragment-onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.v("mudy","Fragment-onStart");
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.v("mudy","Fragment-onActivityCreated");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v("mudy","Fragment-onResume");
     }
 }
--- P1_Day01/fragment/app/src/main/java/fragment/com/mudy/fragment/MainActivity.java
+++ P1_Day01/fragment/app/src/main/java/fragment/com/mudy/fragment/MainActivity.java
@@ -4,14 +4,19 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("mudy","Activity-onCreate");
         setContentView(R.layout.activity_main);
 
+
+
+//动态添加
         FirstFragment first = new FirstFragment();
 
         Bundle bundle = new Bundle();
@@ -24,28 +29,51 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.content,first,"first");
         transaction.commit();
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("mudy","Activity-onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("mudy","Activity-onResume");
     }
 }
 /**  Activity的生命周期
- * onCreate
- * onStart
- * onResume 能够接受用户的事件
- * onStop
+ * 1 onCreate
+ * 6 onStart
+ * 8 onResume 能够接受用户的事件
+ *
+ * onPause 可见到不可见
+ * onStop 准备开始回收
  * onDestroy
  */
 
 
 /** fragment的生命周期
  * add
- * onAttached
- * onCreate
- * onCreateView 产生UI
- * onActivityCreated 宿主activity的onCreate方法已经执行完成
- * onStart()
- * onTrsume()
+ * 2 onAttached
+ * 3 onCreate
+ * 4 onCreateView 产生UI
+ * 5 onActivityCreated 宿主activity的onCreate方法已经执行完成
+ * 7 onStart()
+ * 9 onResume()
  * Fragment is active
- *
- *
- *
- *
+ */
+
+
+/** 为什么没有attached？
+ Activity-onCreate
+ Fragment-onCreate
+ Fragment-onCreateView
+ Fragment-onActivityCreated
+ Activity-onStart
+ Fragment-onStart
+ Activity-onResume
+ Fragment-onResume
  */
