4908d4b3dd99caf58e653f4a8837ca88076ba4e3
coderMudy <2808050775@qq.com>
2018-10-10 11:12:10 +0800

    数据反向传递

--- FirstLineAndroidCode/ActivityTest/app/src/main/java/com/example/mudy/activitytest/FirstActivity.java
+++ FirstLineAndroidCode/ActivityTest/app/src/main/java/com/example/mudy/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -76,6 +77,18 @@ public class FirstActivity extends AppCompatActivity {
         }
 //        return super.onOptionsItemSelected(item);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+        }
     }
 }
 
--- FirstLineAndroidCode/ActivityTest/app/src/main/java/com/example/mudy/activitytest/SecondActivity.java
+++ FirstLineAndroidCode/ActivityTest/app/src/main/java/com/example/mudy/activitytest/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
@@ -18,5 +19,14 @@ public class SecondActivity extends AppCompatActivity {
 //        Log.d("SecondActivity",data);
 
         Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.putExtra("data_return","hello this is data from second");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
     }
 }
