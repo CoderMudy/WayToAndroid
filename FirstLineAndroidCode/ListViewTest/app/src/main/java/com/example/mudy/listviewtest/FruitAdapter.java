381f782b532a8f6344741d19b0c28e409cac15eb
coderMudy <2808050775@qq.com>
2018-10-17 16:28:36 +0800

    listview图片文字显示 自定义adapter

--- FirstLineAndroidCode/ListViewTest/app/src/main/java/com/example/mudy/listviewtest/FruitAdapter.java
+++ FirstLineAndroidCode/ListViewTest/app/src/main/java/com/example/mudy/listviewtest/FruitAdapter.java
@@ -2,9 +2,12 @@ package com.example.mudy.listviewtest;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -23,6 +26,12 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        Fruit fruit = getItem(position);//获取当前项的Fruit实例
+        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+        ImageView imageView = (ImageView)view.findViewById(R.id.fruit_image);
+        TextView textView = (TextView)view.findViewById(R.id.fruit_name);
+        imageView.setImageResource(fruit.getImageId());
+        textView.setText(fruit.getName());
+        return view;
     }
 }
--- FirstLineAndroidCode/ListViewTest/app/src/main/java/com/example/mudy/listviewtest/MainActivity.java
+++ FirstLineAndroidCode/ListViewTest/app/src/main/java/com/example/mudy/listviewtest/MainActivity.java
@@ -8,27 +8,58 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.data;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String[] data = {"Apple","Banana","Orange","Watermelon","Pear","Mango",
-            "Cherry","Grape","Apple","Banana","Orange","Watermelon","Pear","Mango",
-            "Cherry","Grape"};
+//    private String[] data = {"Apple","Banana","Orange","Watermelon","Pear","Mango",
+//            "Cherry","Grape","Apple","Banana","Orange","Watermelon","Pear","Mango",
+//            "Cherry","Grape"};
 
+    private List<Fruit> fruitList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this, android.R.layout.simple_list_item_1,data);
+
+        initFruits();//初始化水果数据
+
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
+
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String name = data[position];
-                Toast.makeText(MainActivity.this,name,Toast.LENGTH_SHORT).show();
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 6; i++){
+            Fruit apple = new Fruit("Apple",R.drawable.apple_pic);
+            fruitList.add(apple);
+
+            Fruit banana = new Fruit("Banana",R.drawable.banana_pic);
+            fruitList.add(banana);
+
+            Fruit orange = new Fruit("Orange",R.drawable.orange_pic);
+            fruitList.add(orange);
+
+            Fruit pear = new Fruit("梨子",R.drawable.pear_dic);
+            fruitList.add(pear);
+
+            Fruit waterMalen = new Fruit("西瓜",R.drawable.watermelon_pic);
+            fruitList.add(waterMalen);
+
+            Fruit cherry = new Fruit("草莓",R.drawable.cherry_pic);
+            fruitList.add(cherry);
+        }
     }
 }
